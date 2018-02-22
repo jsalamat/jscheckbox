10c1a59157dde1aed21afe344ec8ea6d467bd0b3
jsalamat <salamat_jon@yahoo.com>
2018-02-22 09:02:23 -0500

    check first put in var and second to be lastChecked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,11 +5,23 @@ const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 // console.log(checkboxes);
 
 // listen for an event once click or change
+// when check first one to be put in variable
+// when check the second one and holding  shift need to know the last one click
+let lastChecked;
+
 function handleCheck(e) {
-	console.log(e);
+	//console.log(e);
+	lastChecked = this;
+	console.log(lastChecked);
+	//check if the shift key down
+	// and check that they are checking the box
+	if (e.shiftKey && this.checked) {
+		//loop over the checkbox 
+		// and look for first one check then lastCheck
+	}
 };
 
 // loop over each checkbox
 // and listen for a click
-// when click to run handleCheck
+// whgit en click to run handleCheck
 checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck))
