c51e2b135ae814bf94b2f225488c6301d3279f28
jsalamat <salamat_jon@yahoo.com>
2018-02-22 08:41:25 -0500

    selct all checkboxes and listen for e when click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,15 @@
-const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+//select all the checkboxes and listened for event when check
 
-console.log(checkboxes);
+//select all the checkboxes
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+// console.log(checkboxes);
+
+// listen for an event once click or change
+function handleCheck(e) {
+	console.log(e);
+};
+
+// loop over each checkbox
+// and listen for a click
+// when click to run handleCheck
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck))
